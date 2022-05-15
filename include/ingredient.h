cfb4b8729dcacb7a507c1a52623c4f2ee86ff4fd
Mohammad Mahdi Sharifian <mahdi.sharif20001@gmail.com>
2022-05-15 20:46:47 +0000

    implement ingredients

--- include/ingredient.h
+++ include/ingredient.h
@@ -1,4 +1,24 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
 
+class Ingredient
+{
+public:
+    double get_price_unit() { return price_unit;}
+    size_t get_units() { return units; }
+    virtual std::string get_name() = 0; 
+
+    double price() {return units * price_unit;}
+    
+
+protected:
+    Ingredient(double _price_unit, size_t _units)
+    :price_unit{_price_unit}
+    ,units{_units} {}
+
+    double price_unit;
+    size_t units;
+    std::string name;
+};
+
 #endif // INGREDIENT_H
