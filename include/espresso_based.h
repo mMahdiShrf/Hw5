3f6a325a226c209b806cc2e13da91bcd98515db1
Mohammad Mahdi Sharifian <mahdi.sharif20001@gmail.com>
2022-05-16 12:53:43 +0000

    implementing espresso_baded except brew

--- include/espresso_based.h
+++ include/espresso_based.h
@@ -1,4 +1,31 @@
 #ifndef ESPRESSO_BASED_H
 #define ESPRESSO_BASED_H
 
+#include <string>
+#include <vector>
+#include "ingredient.h"
+#include "sub_ingredients.h"
+
+class EspressoBased
+{
+public:
+    virtual std::string get_name() = 0;
+    virtual double price() = 0;
+
+    //void brew();
+    std::vector<Ingredient*>& get_ingredients();
+    void operator=(EspressoBased& esp);
+
+    ~EspressoBased();
+
+protected:
+    EspressoBased();
+    EspressoBased(const EspressoBased& esp);
+    void operator=(const EspressoBased& esp);
+
+    std::vector<Ingredient*> ingredients;
+    std::string name;
+
+};
+
 #endif // ESPRESSO_BASED_H
--- src/espresso_based.cpp
+++ src/espresso_based.cpp
@@ -1,1 +1,28 @@
 #include "espresso_based.h"
+
+EspressoBased::EspressoBased()
+:ingredients{}
+,name{}
+{}
+
+EspressoBased::~EspressoBased()
+{
+    for(const auto& i : ingredients)
+    delete i;
+    ingredients.clear();
+}
+
+std::vector<Ingredient*>& EspressoBased::get_ingredients()
+{
+    return ingredients;
+}
+
+EspressoBased::EspressoBased(const EspressoBased& esp)
+:ingredients {std::vector<Ingredient*>{esp.ingredients}}
+,name{esp.name}
+{}
+void EspressoBased::operator= (EspressoBased& esp)
+{
+    ingredients = esp.ingredients;
+    name = esp.name;
+}
