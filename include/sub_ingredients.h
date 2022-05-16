cff0962114b336bb9e79c704b48e205e48dbdd33
Mohammad Mahdi Sharifian <mahdi.sharif20001@gmail.com>
2022-05-16 10:35:32 +0000

    implement Sub_ingredient based on challange

--- include/sub_ingredients.h
+++ include/sub_ingredients.h
@@ -1,4 +1,29 @@
 #ifndef SUB_INGREDIENTS_H
 #define SUB_INGREDIENTS_H
 
+#include <iostream>
+#include "Ingredient.h"
+
+
+#define DEFCLASS(drived_class_name,price_unit)\
+class drived_class_name: protected Ingredient\
+{ \
+    public:\
+    drived_class_name(size_t units) : Ingredient{price_unit, units}\
+    {\
+        this->name = typeid(*this).name() + 1;\
+    }\
+    virtual std::string get_name() {return this->name;}\
+};
+
+DEFCLASS(Cinnamon, 5);
+DEFCLASS(Chocolate, 5);
+DEFCLASS(Sugar, 1);
+DEFCLASS(Cookie, 10);
+DEFCLASS(Espresso, 15);
+DEFCLASS(Milk, 10);
+DEFCLASS(MilkFoam, 5);
+DEFCLASS(Water, 1);
+
+
 #endif // SUB_INGREDIENTS_H
